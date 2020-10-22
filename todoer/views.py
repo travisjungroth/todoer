792cf6be431112d6da531223a53b3756de1dedd8
travisjungroth <jungroth@gmail.com>
2020-10-21 22:50:56 -0500

    Add fibonacci streaks

--- todoer/views.py
+++ todoer/views.py
@@ -24,12 +24,37 @@ class WeekScores(TemplateView):
             if task.completed:
                 completed[task.template] += 1
 
+        d = {}
+        for task in Task.objects.filter(date__lt=today).order_by('template__order', 'date'):
+            d.setdefault(task.template, []).append(task.completed)
+
         scores = []
         for template in total:
-            scores.append({'name': template.name, 'completed': completed[template], 'total': total[template]})
+            streak, goal = f_streak(d[template])
+            scores.append(
+                {
+                    'name': template.name,
+                    'completed': completed[template],
+                    'total': total[template],
+                    'streak': streak,
+                    'goal': goal,
+                }
+            )
 
         context = super().get_context_data(**kwargs)
         context['scores'] = scores
         context['overall'] = f'{sum(completed.values())}/{sum(total.values())}'
-        context['percent'] = f'{round(sum(completed.values())/sum(total.values()) * 100)}%'
+        context['percent'] = f'{round(sum(completed.values()) / sum(total.values()) * 100)}%'
         return context
+
+
+def f_streak(bools):
+    fibonacci = iter((1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987))
+    goal = next(fibonacci)
+    streak = 0
+    for b in bools:
+        if streak >= goal:
+            streak = 0
+            goal = next(fibonacci)
+        streak = streak + 1 if b else 0
+    return streak, goal
