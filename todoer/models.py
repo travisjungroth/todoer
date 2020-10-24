1f4f5e963efec4e374bcc31ee02bc72af0efed67
travisjungroth <jungroth@gmail.com>
2020-10-24 07:37:22 -0500

    Add User model everywhere

--- todoer/models.py
+++ todoer/models.py
@@ -1,12 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.utils.timezone import now
 from ordered_model.models import OrderedModel
 
+User = get_user_model()
+
 
 class Schedule(models.Model):
     name = models.CharField(max_length=255)
     days = ArrayField(base_field=models.IntegerField())
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -18,6 +22,7 @@ class TaskTemplate(OrderedModel):
     morning = models.BooleanField()
     reminder_time = models.CharField(max_length=10, help_text='like 9pm', blank=True, default='')
     active = models.BooleanField(default=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     class Meta(OrderedModel.Meta):
         pass
@@ -33,6 +38,7 @@ class Task(models.Model):
     order = models.IntegerField()
     todoist_id = models.BigIntegerField()
     template = models.ForeignKey(TaskTemplate, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['-date', 'order']
