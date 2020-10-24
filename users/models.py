8d7ebc8f8642253324f1c46603e558b97d5ea63a
travisjungroth <jungroth@gmail.com>
2020-10-24 07:56:38 -0500

    Add UserProfile

--- users/models.py
+++ users/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    api_token = models.CharField(default='', blank=True, max_length=255)
