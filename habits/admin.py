8752e83292e2e39b9db9a54ad9e96fd6c8ee975b
travisjungroth <jungroth@gmail.com>
2020-10-26 09:26:38 -0500

    Add user admin

--- habits/admin.py
+++ habits/admin.py
@@ -1,7 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 from ordered_model.admin import OrderedModelAdmin
 
 from habits import models
+
+
+@admin.register(get_user_model())
+class UserAdmin(UserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "date_joined")
+    ordering = ("username",)
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {"classes": ("wide",), "fields": ("password1", "password2")},),
+    )
 
 
 @admin.register(models.Habit)
