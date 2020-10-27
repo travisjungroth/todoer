55cc093c25271619232be6090d5254be8e86f145
travisjungroth <jungroth@gmail.com>
2020-10-26 20:57:43 -0500

    Fix links

--- habits/urls.py
+++ habits/urls.py
@@ -1,3 +1,4 @@
+from allauth.account.views import logout
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
@@ -5,9 +6,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
     path('', views.Home.as_view(), name='home'),
+    path('admin/', admin.site.urls),
+    path('accounts/todoist/', include('todoist_oauth2.urls')),
+    path('accounts/logout/', logout, name="logout"),
     path('report/', views.WeekScores.as_view(), name='report'),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
