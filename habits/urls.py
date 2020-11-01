0f9db5404e550a56ab80732c54e49ad3102495d1
travisjungroth <jungroth@gmail.com>
2020-11-01 18:46:23 -0500

    Fix URL

--- habits/urls.py
+++ habits/urls.py
@@ -8,8 +8,8 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('admin/', admin.site.urls),
-    path('accounts/todoist/', include('todoist_oauth2.urls')),
     path('accounts/logout/', logout, name="logout"),
+    path('accounts/', include('todoist_oauth2.urls')),
     path('habits/', views.HabitView.as_view(), name='habits'),
     path('report/', views.WeekScores.as_view(), name='report'),
     path('__debug__/', include(debug_toolbar.urls)),
