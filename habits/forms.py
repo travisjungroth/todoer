36f5817fb2ca8b6cb8bc726c7e927be11d3a3884
travisjungroth <jungroth@gmail.com>
2020-11-02 14:02:50 -0500

    Change to checkboxes and add delete cell

--- habits/forms.py
+++ habits/forms.py
@@ -4,10 +4,23 @@ from django import forms
 
 from habits.models import Habit
 
+
+class HabitForm(forms.ModelForm):
+    days = forms.TypedMultipleChoiceField(
+        choices=enumerate('MTWTFSS'),
+        coerce=int,
+        empty_value=None,
+        widget=forms.CheckboxSelectMultiple(),
+    )
+
+    class Meta:
+        model = Habit
+        fields = ['name', 'days', 'morning', 'reminder_time', 'active', 'show_streaks']
+
+
 HabitFormSet = forms.modelformset_factory(
     Habit,
-    fields=['name', 'days', 'morning', 'reminder_time', 'active', 'show_streaks'],
-    can_delete=True,
+    form=HabitForm,
     extra=1,
 )
 
@@ -15,5 +28,5 @@ HabitFormSet = forms.modelformset_factory(
 class HabitFormSetHelper(FormHelper):
     def __init__(self, form=None):
         super().__init__(form)
-        self.template = 'bootstrap4/table_inline_formset.html'
+        self.template = 'table_inline_formset.html'
         self.add_input(Submit("submit", "Save"))
